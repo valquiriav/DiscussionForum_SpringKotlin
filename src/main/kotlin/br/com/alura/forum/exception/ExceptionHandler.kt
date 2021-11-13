12ffaba2670ef157840d61ff783a828413c05556
valquiriaoliveira <valquiria.oliveira@empiricus.com.br>
2021-11-12 22:14:35 -0300

    Exception handling

---
+++ src/main/kotlin/br/com/alura/forum/exception/ExceptionHandler.kt
@@ -0,0 +1,27 @@
+package br.com.alura.forum.exception
+
+import br.com.alura.forum.dto.ErrorView
+import javax.servlet.http.HttpServletRequest
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class ExceptionHandler {
+
+    @ExceptionHandler(NotFoundException::class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    fun handleNotFound(
+            exception: NotFoundException,
+            request: HttpServletRequest
+        ): ErrorView {
+        return ErrorView(
+            status = HttpStatus.NOT_FOUND.value(),
+            error = HttpStatus.NOT_FOUND.name,
+            message = exception.message,
+            path = request.servletPath
+        )
+    }
+
+}
