08e738d8f969d5e68b95f01c47b1a898bec7b977
valquiriaoliveira <valquiria.oliveira@empiricus.com.br>
2021-11-16 15:56:24 -0300

    Exception handling

--- src/main/kotlin/br/com/alura/forum/repository/CursoRepository.kt
+++ src/main/kotlin/br/com/alura/forum/repository/CursoRepository.kt
@@ -1,7 +1,8 @@
 package br.com.alura.forum.repository
 
-import br.com.alura.forum.model.Topico
+import br.com.alura.forum.model.Curso
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface TopicoRepository: JpaRepository<Topico, Long> {
+interface CursoRepository: JpaRepository<Curso, Long> {
+
 }
--- src/main/kotlin/br/com/alura/forum/repository/UsuarioRepository.kt
+++ src/main/kotlin/br/com/alura/forum/repository/UsuarioRepository.kt
@@ -1,7 +1,7 @@
 package br.com.alura.forum.repository
 
-import br.com.alura.forum.model.Topico
+import br.com.alura.forum.model.Usuario
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface TopicoRepository: JpaRepository<Topico, Long> {
+interface UsuarioRepository: JpaRepository<Usuario, Long> {
 }
