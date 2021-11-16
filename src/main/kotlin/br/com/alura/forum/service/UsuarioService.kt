568473942004e1e0a21b5c4db99a8ffa2011f361
valquiriaoliveira <valquiria.oliveira@empiricus.com.br>
2021-11-16 19:26:50 -0300

    Creating repository classes

--- src/main/kotlin/br/com/alura/forum/service/UsuarioService.kt
+++ src/main/kotlin/br/com/alura/forum/service/UsuarioService.kt
@@ -1,20 +1,12 @@
 package br.com.alura.forum.service
 
 import br.com.alura.forum.model.Usuario
+import br.com.alura.forum.repository.UsuarioRepository
 import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
-class UsuarioService(var usuarios: List<Usuario>) {
-
-    init {
-        val usuario = Usuario(
-            id = 1,
-            nome = "Ana",
-            email = "ana@email.com.br"
-        )
-        usuarios = Arrays.asList(usuario)
-    }
+class UsuarioService(private val repository: UsuarioRepository) {
 
     fun buscarPorId(id: Long): Usuario {
         return usuarios.stream().filter({
